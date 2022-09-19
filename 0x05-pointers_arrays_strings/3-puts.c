ffd3d77e9cea648bd46dbffcca122aec0e7eddef
gnmbuto <legacydenaf@gmail.com>
2022-09-19 13:31:29 -0700

    Function that prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  *_puts - prints a string
  * @str: string to print
+ *
  * Description: prints a string
+ * On success: returns no error
  */
 
 void _puts(char *str)
