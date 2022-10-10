cac3ee6b32198771fece98dd9028b7a60fc2ce81
gnmbuto <legacydenaf@gmail.com>
2022-10-10 11:35:07 -0700

    Thoth

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
-/**                                                                                                                                  
- * _strspn - Gets the length of a prefix substring.                                                                                  
- * @s: The string to be searched.                                                                                                    
- * @accept: The prefix to be measured.                                                                                               
- *                                                                                                                                   
- * Return: The number of bytes in s which                                                                                            
- *         consist only of bytes from accept.                                                                                        
- */                                                                                                                                  
-unsigned int _strspn(char *s, char *accept)                                                                                          
-{                                                                                                                                    
-        unsigned int bytes = 0;                                                                                                      
-        int index;                                                                                                                   
-                                                                                                                                     
-        while (*s)                                                                                                                   
-        {                                                                                                                            
-                for (index = 0; accept[index]; index++)                                                                              
-                {                                                                                                                    
-                        if (*s == accept[index])                                                                                     
-                        {                                                                                                            
-                                bytes++;                                                                                             
-                                break;                                                                                               
-                        }                                                                                                            
-                                                                                                                                     
-                        else if (accept[index + 1] == '\0')                                                                          
-                                return (bytes);                                                                                      
-                }                                                                                                                    
-                                                                                                                                     
-                s++;                                                                                                                 
-        }                                                                                                                            
-                                                                                                                                     
-        return (bytes);                                                                                                              
-}
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s: char pointer
+ * @accept: char pointer
+ * Return: s
+ */
 
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int count1, count2;
+
+	for (count1 = 0; s[count1] != '\0'; count1++)
+	{
+		for (count2 = 0; accept[count2] != '\0'; count2++)
+		{
+			if (s[count1] == accept[count2])
+			{
+				break;
+			}
+		}
+			if (!(accept[count2]))
+			{
+				break;
+			}
+	}
+	return (count1);
+}
+*/
