bebdb49358582766325e784b194a53e7de0ea126
gnmbuto <legacydenaf@gmail.com>
2022-10-10 11:56:50 -0700

    dici

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- *strpbk - searches astring for bytes
- *returns a pointer to s or
- *NULL
+ * _strpbrk -  searches a string for any of a set of bytes.
+ *
+ * @s: char pointer
+ *
+ * @accept: char pointer
+ *
+ * Return: string
  */
 
 char *_strpbrk(char *s, char *accept)
