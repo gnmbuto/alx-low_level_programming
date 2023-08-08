e0f2963cbe05bab8609c83b57e22d454450318d1
gnmbuto <legacydenaf@gmail.com>
2023-08-08 00:45:37 -0700

    Puts_Recursive func

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string followedby a new line character
+ * s- character printer to be printed and is also the parameter
+ * @gnmbuto
+ */
+void _puts_recursion(char *s){
+	if (s[0] != '\0'){
+		_putchar(s[0]);
+		_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
